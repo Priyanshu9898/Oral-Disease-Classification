93f1a290e825cf01f2ad0712a90fcf16ee987d0e
Priyanshu9898 <priyanshu.mce20@sot.pdpu.ac.in>
2024-01-10 19:14:12 +0530

    some changes

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,11 @@
-import { Button } from '@/components/ui/button'
+
+import ImageInput from '@/components/Home/ImageInput'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-      Home
+      <ImageInput />
     </div>
   )
 }
