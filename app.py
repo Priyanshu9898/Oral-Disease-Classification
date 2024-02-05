8a2b94b7c6844d9014c76dc7b43fe4fbfde035b4
Priyanshu9898 <priyanshu.mce20@sot.pdpu.ac.in>
2024-02-05 20:49:14 +0530

    Frontend url added for cors

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ from flask_swagger_ui import get_swaggerui_blueprint
 app = Flask(__name__)
 CORS(app)
 
-allowed_origins = ['https://oral-disease-classification.vercel.app', 'http://localhost:3000', 'http://127.0.0.1:3000',
+allowed_origins = ['https://meta-doctor.vercel.app', 'https://meta-doctor-git-main-priyanshumalaviya9210-gmailcom.vercel.app', 'https://meta-doctor-nwxeblfpx-priyanshumalaviya9210-gmailcom.vercel.app', 'https://oral-disease-classification.vercel.app', 'http://localhost:3000', 'http://127.0.0.1:3000',
                    'https://oral-disease-classification.vercel.app/']
 
 SWAGGER_URL = '/swagger'
